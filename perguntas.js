56261401383443892bc005e28173614fa81ecf68
digresco <166826138+nigresco@users.noreply.github.com>
2024-10-11 11:43:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O Que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O Que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'lingua inglesa',
+    'como se diz oi em inglês?',
+    'oi em inglês é hi'
+)
+
+
